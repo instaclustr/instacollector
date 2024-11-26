909a0b58ddf8e6948a49d49d02f4004002fade80
Mike Bloom <cmdrchkn@gmail.com>
2024-11-26 13:38:05 -0500

    Updates to address review items for upstream PR#9

--- kafka/README.md
+++ kafka/README.md
@@ -1,68 +1,64 @@
+# Instacollector for Kafka
+This tool is used to collect information from a Kafka cluster to add in problem diagnosis and cluster health review.
 
-This tool is used to collect information from a Kafka cluster to add in problem diagnosis or review.
+## Design
+There are two scripts used in Instacollector for Kafka:
 
-Note: 
-* This won't work on versions before kafka 2
-* User is requested to change the path values in `node_collector.sh` before running any of the scripts, as by default the script uses `Kafka configuration file locations`, `data directory location`, and other setting locations as per **Apache Kafka** default setup.
+1. `node_collector.sh`: Executes Linux and Kafka commands and copies configuration and log files required for cluster health checks on each node. The user may need to modify the `KAFKA_HOME` path inside the script as per their configuration, the default value used is: `KAFKA_HOME=/opt/kafka`
 
-# Design info:
-There are two scripts used in instacollector tool for Kafka.
+2. `cluster_collector.sh`: Executes `node_collector.sh` on each Kafka node from a central machine with access to each node. `cluster_collector.sh` uses the following information:
+  - **Environment**: [SSH|Docker]
+    - for `SSH` environments the script will scp the node_collector and execute it. The script will additionaly ask for:
+      - SSH username
+      - path to SSH identity file
+    - for `Docker` environments the script will use `docker exec` to run the node_collector. The script will additionaly ask for:
+      - Docker Home directory: a path inside the containter where we can store the script and generated files (defaults to `/tmp`)
+  - **Command Config**
+    - path to the command config file to use for Kafka CLI tools (optional)
+  - **Peers File**
+    - file with a list of IPs/hostnames/containers to connect to
 
-1. `node_collector.sh`: supposed to be executed on each Kafka node. It executes Linux and Kafka commands and copies configuration and log files required for cluster health checks. The user needs to modify the `KAFKA_HOME` path inside the script as per their configurations, the default value used is:
-```
-KAFKA_HOME=/opt/kafka
-```
-2. `cluster_collector.sh`: to be executed on a machine connected to Kafka cluster e.g. user laptop with a running Docker or a running VM. It executes node_collector.sh on each Kafka node using ssh. The cluster_collector.sh requires 4 user inputs :
-```
-   * Enter your kafka environment (SSH/Docker) :
-     * [If SSH]
-       * Enter username for login on Kafka cluster nodes (Press Enter for default admin) :
-       * Enter Identity file path: (the ssh key file in your local machine which is used to connect to the VMs)
-     * [If Docker]
-       * Enter Docker home directory:
-   * Enter path of the command config file: (Kafka command-config file location on the Kafka brokers)
-   * Enter file containing ip addresses/host/container names of Kafka cluster nodes: (the hosts file in your local machine)
-```
-*******************
-# Execution settings:
+
+## Execution
 The `cluster_collector.sh` has setting of connecting to cluster nodes using the provided ssh key file or an id file.
 
-If the ssh key has passphrase enabled then please use `ssh-agent` & `ssh-add` commands to add the passphrase before running `cluster_collector.sh` script.
-example:
-```
-Use the following command to execute the ssh-agent commands and enable ssh-agent if not already done so:
+If the ssh key has a passphrase enabled then please use `ssh-agent` & `ssh-add` commands to add the passphrase before running `cluster_collector.sh` script with `eval "$(ssh-agent -s)"`. By default, `ssh-agent` should automatically load the keys in `~/.ssh` when started, if needed you can manually add a key from elsewhere with: `ssh-add path/to/key`
 
--> eval `ssh-agent`
-
-By default, ssh agent automatically should load the keys from .ssh folder, but you can do so manually too.
-
--> ssh-add path/to/key
-
-```
-
-If there is another method required for `ssh`, user is requested to change the script as applicable.
-
-Alternatively, the `node_collector.sh` can also be executed on individual nodes if `cluster_collector.sh` is not useful in any case.
+If another method is required for connecting to the target nodes with `ssh`, changes to the script may be required, alternatively the `node_collector.sh` can also be executed on individual nodes manually.
 
 
-Below are the Kafka & Zookeeper related files which will be copied from different nodes:
-```
-Kafka Broker Files                |  Zookeeper Files
-**********************************|***********************
-server.properties                 |  zookeeper.properties
-server.log                        |  zoo.cfg
-kafkaServer.out                   |  log4j.properties
-kafka-authorizer.log              |  zoo.log
-controller.log                    |  zookeeper_jaas.conf
-state-change.log                  |  zookeeper.out
-kafka_server_jaas.conf            |       
-kafka-topics/.sh                  |
-kafka-topics/.sh                  |
-kafka-broker-api-versions/.sh     |
-kafka-consumer-groups/.sh         |
-server.properties
-```
+## Collected Information
 
-**Note:** The scripts should be executed on bash shell.
+### Files
+Kafka & Zookeeper related files which will be copied:
+
+| Kafka Broker Files             |  Zookeeper Files      |
+| ------------------------------ | --------------------- |
+| server.properties              |  zookeeper.properties |
+| server.log                     |  zoo.cfg              |
+| kafkaServer.out                |  log4j.properties     |
+| kafka-authorizer.log           |  zoo.log              |
+| controller.log                 |  zookeeper_jaas.conf  |
+| state-change.log               |  zookeeper.out        |
+| kafka_server_jaas.conf         |                       |
+| broker.properties              |                       |
+| controller.properties          |                       |
+
+### Kafka CLI Commands
+
+- `kafka-topics.sh --version`
+- `kafka-metadata-quorum.sh --describe`
+- `kafka-topics.sh --describe`
+- `kafka-broker-api-versions.sh`
+- `kafka-consumer-groups.sh --describe --all-groups --verbose`
+
+
+## Notes
+- These scripts target Kafka 3 and newer
+- The target paths in `node_collector.sh` may need to be updated depending on the install location of Kafka/Zookeeper
+- Scripts are intended to be run with `bash`
+
+
+## Support Status
 
 Please see https://www.instaclustr.com/support/documentation/announcements/instaclustr-open-source-project-status/ for Instaclustr support status of this project
--- kafka/cluster_collector.sh
+++ kafka/cluster_collector.sh
@@ -1,67 +1,89 @@
 #!/bin/bash
 
-##********************************************************************************************************************
-##********************************************************************************************************************
-## The purpose of this tool is to extract kafka & zookeeper related configuration and log files for troubleshooting. 
-## Following are the list of files that are extracted. Please note that not all files exists in an environment. 
-## All properties with the word "password" in it are replaced with "***" 
-#=============================================================#
-# kafka files and the path variables where they are expected
+#==========================#
+# --       Usage        -- #
+#==========================#
+# ./cluster_collector.sh
+# bash cluster_collector.sh
+
+
+#==========================#
+# --      Purpose       -- #
+#==========================#
+# This tool will copy Kafka & Zookeeper/KRaft related configuration and log files for troubleshooting or cluster health review.
+
+
+#==========================#
+# --       Notes        -- #
+#==========================#
+# - Not all files listed below will exist for all clusters (e.g. KRaft and Zookeeper are mutually exclusive) but the 
+#     script will still attempt to collect for both
+# - Properties files with the word "password" in them will be redacted with "***"
+
+
+#==========================#
+# --   Expected Paths   -- #
+#==========================#
 # BROKER_CONFIG_PATH
-#   server.properties
+#   └─server.properties
+#
 # BROKER_LOG_PATH
-#   server.log
-#   kafkaServer.out
-#   kafka-authorizer.log
-#   controller.log
-#   state-change.log
+#   ├─server.log
+#   ├─kafkaServer.out
+#   ├─kafka-authorizer.log
+#   ├─controller.log
+#   └─state-change.log
+#
 # BROKER_JAAS_CONFIG
-#   kafka_server_jaas.conf
+#   └─kafka_server_jaas.conf
+#
 # ZOOKEEPER_CONFIG
-#   zookeeper.properties
-#   zoo.cfg
-#   log4j.properties
+#   ├─zookeeper.properties
+#   ├─zoo.cfg
+#   └─log4j.properties
+#
 # ZOOKEEPER_LOG_PATH
-#   zoo.log
+#   ├─zoo.log
+#   └─zookeeper.out
+#
 # ZOOKEEPER_JAAS_CONFIG
-#   zookeeper_jaas.conf
-# ZOOKEEPER_LOG_PATH
-#   zookeeper.out
+#   └─zookeeper_jaas.conf
+#
+# KRAFT_CONFIG_PATH
+#   ├─server.properties
+#   ├─controller.properties
+#   └─broker.properties
+#
 # BROKER_BIN_PATH
-#   kafka-topics/.sh
-#   kafka-topics/.sh
-#   kafka-broker-api-versions/.sh
-#   kafka-consumer-groups/.sh 
-#=============================================================#      
-##
-## In addition to the files above the script also extract the following OS related information - 
-## 1. file system & directory size 
-## 2. io stats
-## 3. file descriptors
-## 4. cpu & memory 
-## 5. contents of the hosts file 
-## 6. output of kafka-topics.sh topic describe 
-## 
-##********************************************************************************************************************
-##********************************************************************************************************************
-## Last Modification Date : 10/29/2021
-## Description		 : Script functionality enhanced to add information related to iostat, df, file descriptor
-##                          cpu & memory info 
-##********************************************************************************************************************
-##********************************************************************************************************************
+#   ├─kafka-topics/.sh
+#   ├─kafka-topics/.sh
+#   ├─kafka-broker-api-versions/.sh
+#   └─kafka-consumer-groups/.sh
+#
+# -- Other information gathered
+# - Filesystem & data directory size 
+# - IO Stats
+# - File Descriptor limits
+# - CPU & Memory usage 
+# - Contents of the hosts file 
+# - Output of various Kafka CLI tools  
 
-clear
 
-#GLOBAL VARIABLES
+#==========================#
+# -- Collection Options -- #
+#==========================#
+# -- Global Variables
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 INFO_DIR=/tmp/InstaCollection_$(date +%Y%m%d%H%M)
+clear
 
-#Collect environment info (VM/Docker)
-read -p "Enter your Kafka environment (SSH/Docker) :" kenv
+# -- Environment info (SSH/Docker)
+read -p "Enter your Kafka environment [SSH|Docker] :" kenv
 
+# -- SSH info
 if [[ "${kenv}" == "SSH" ]]; then
-  #Collect user info.
-  read -p "Enter SSH username for login on Kafka cluster nodes (Press Enter for default admin) :" user
+  # -- Collect user info
+  read -p "Enter SSH username for login on Kafka cluster nodes: [admin] " user
   [ -z "${user}" ] && user='admin'
 
   read -p "Enter Identity file path:" id_file
@@ -70,23 +92,24 @@ if [[ "${kenv}" == "SSH" ]]; then
       exit 1
   fi
 
+# -- Docker info
 elif [[ "${kenv}" == "Docker" ]]; then
-  echo "Default directory to store the files is : /tmp"
-  echo "Hit Enter key to choose the default path, else please enter a specific path."
-  echo " Please make sure the directory is writable."
-  read -p "Enter directory path to store the output:" docker_home
-
-  if [ -z "docker_directory" ]; then
+  read -p  "Specify a writable directory inside the container to store the output: [/tmp]" docker_home
+  if [ -z "docker_home" ]; then
     docker_home= "/tmp"
-    exit 1
   fi
+
+# -- Unknown selection
 else
   echo "Invalid value for environment"
   exit 1
 fi
 
-echo  "Please make sure you have a command config files inside the container/VM to make use of it"
-read -p "Enter path of the command config file:" config_file
+# -- Command Config
+read -p "Enter path of the command config file: [none]" config_file
+if [ -z "${config_file}" ]; then
+    unset config_file
+  fi
 
 read -p "Enter file containing ip addresses/host/container names of Kafka cluster nodes:" peers_file
 if [[ ! -f ${peers_file} || ! -s ${peers_file} ]]; then
@@ -94,18 +117,28 @@ if [[ ! -f ${peers_file} || ! -s ${peers_file} ]]; then
     exit 1
 fi
 
+echo "Using environment $kenv"
 
-echo "environment $kenv"
 
-#Execute the node_collector on each node or container
+#==========================#
+# --    Collect Data    -- #
+#==========================#
+# -- Execute the node_collector with SSH
 if [ "$kenv" == "SSH" ]; then
   while read peer 
   do 
           if [[ -z "$peer" ]]; then
             break
           fi
-          ssh -i $id_file $user@$peer "bash -s" < node_collector.sh $peer $config_file &
+
+          if [[ -z "$config_file"]]; then 
+            ssh -i $id_file $user@$peer "bash -s" < node_collector.sh -ip $peer &
+          else
+            ssh -i $id_file $user@$peer "bash -s" < node_collector.sh -ip $peer -c $config_file &
+          fi
   done < "$peers_file"
+
+# -- Execute the node_collector with Docker
 else
   while read peer
   do
@@ -113,19 +146,24 @@ else
         break
       fi
       echo "Copying file node_collector.sh to container" 
-      docker cp ./node_collector.sh $peer:docker_directory/
-      docker exec $peer sh "docker_directory/node_collector.sh -ip $peer -c $config_file" &
+      docker cp ./node_collector.sh $peer:$docker_home/
+      
+      if [[ -z "$config_file"]]; then 
+        docker exec $peer sh "$docker_home/node_collector.sh -ip $peer" &
+      else
+        docker cp $config_file $peer:$docker_home/
+        docker exec $peer sh "$docker_home/node_collector.sh -ip $peer -c $config_file" &
+      fi
   done < "$peers_file"
 fi
 
-#waiting for all node_collectors to complete
+# -- Wait for all node_collectors to complete
 wait
 
 mkdir $INFO_DIR
 
-#copy the data from each node/container
-
-if [ "$kenv" == "vm" ]; then
+# -- Copy the data from each node with SSH
+if [ "$kenv" == "SSH" ]; then
   while read peer 
   do 
       if [[ -z "$peer" ]]; then
@@ -135,6 +173,8 @@ if [ "$kenv" == "vm" ]; then
       scp -i $id_file $user@$peer:/tmp/InstaCollection.tar.gz $INFO_DIR/$peer/InstaCollection_$peer.tar.gz &
 
   done < "$peers_file"
+
+# -- Copy the data from each container with Docker
 else
   while read peer
   do
@@ -142,18 +182,18 @@ else
         break
       fi
       mkdir $INFO_DIR/$peer
-      docker cp $peer:/tmp/InstaCollection.tar.gz $INFO_DIR/$peer/InstaCollection_$peer.tar.gz & 
+      docker cp $peer:$docker_home/InstaCollection.tar.gz $INFO_DIR/$peer/InstaCollection_$peer.tar.gz & 
 
   done < "$peers_file"
   
 fi
 
-#waiting for all scp to complete
+# -- Wait for all copy jobs to complete
 wait
 
-#compress the info directory 
+# -- Compress the info directory 
 result_file=/tmp/InstaCollection_$(date +%Y%m%d%H%M).tar.gz
 tar -zcf $result_file -C $INFO_DIR .
 rm -r $INFO_DIR
 
-echo "Process complete. File generated : " $result_file
+echo "Process complete. File generated: " $result_file
